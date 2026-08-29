8ce282c97e49b318de5a241bda1fb87fec9ec87b
Coolio <zap1486@amerike.edu.mx>
2026-08-29 16:37:49 -0600

    tarea1

---
+++ datos_personales.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main (){
+    printf("Julio Esteban Serrano De Jesus\n");
+    printf("17\n");
+    printf("julio.esteban.serrano@gmail.com\n");
+    printf("Ostras\n");
+
+}
